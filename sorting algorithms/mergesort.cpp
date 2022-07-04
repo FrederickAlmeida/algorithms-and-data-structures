d50febe3bd6e3ef61db9ee7c359dc68c69d94dcc
Frederick Almeida <96616444+FrederickAlmeida@users.noreply.github.com>
2022-07-04 14:35:51 -0300

    Update mergesort.cpp

--- sorting algorithms/mergesort.cpp
+++ sorting algorithms/mergesort.cpp
@@ -57,11 +57,11 @@ int main() {
 
         mergesort(parray, left, right);
 
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++) {
             if (i < (size - 1)) {
                 cout << parray[i] << " ";
-            }
-            else cout << parray[i] << "\n";
+            } else cout << parray[i] << "\n";
+        }
 
         delete[] parray;
     }
